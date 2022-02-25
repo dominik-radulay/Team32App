0fa1a1ee522fa82b0db070717cbc77ce24f2abe0
dominik-radulay <59743640+dominik-radulay@users.noreply.github.com>
2022-02-25 10:13:45 +0000

    Add project files.

---
+++ Team32App/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Team32App.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Team32App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Team32App.ViewModels;
+using Xamarin.Forms;
+
+namespace Team32App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
