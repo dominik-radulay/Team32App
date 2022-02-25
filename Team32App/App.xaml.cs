3a1e21dff988c60e7b8d3d6784f0046117d946d1
dominik-radulay <59743640+dominik-radulay@users.noreply.github.com>
2022-02-25 10:13:45 +0000

    Add project files.

---
+++ Team32App/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Team32App.Services;
+using Team32App.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Team32App
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
