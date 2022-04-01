0fd30e2fdcb14a1bf851ad63f47c76a5ba3aac57
Lukas Kazarian <loukaskazarian@gmail.com>
2022-04-01 16:47:58 +0100

    test

---
+++ Team32App/Views/CommunicationLevels/Suffix.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Team32App.Views.Communication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Suffix : ContentPage
+    {
+        public Suffix()
+        {
+            InitializeComponent();
+        }
+    }
+}
