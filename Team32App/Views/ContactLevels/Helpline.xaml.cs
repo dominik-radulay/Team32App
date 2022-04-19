36e4175c38057ca1561ee4b20f6e1a1509f9b613
Sam Grieveson <w19013338@northumbria.ac.uk>
2022-04-19 17:45:54 +0100

    Added Helpline Dialer

--- Team32App/Views/ContactLevels/Helpline.xaml.cs
+++ Team32App/Views/ContactLevels/Helpline.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,22 @@ namespace Team32App.Views.ContactLevels
         public Helpline()
         {
             InitializeComponent();
+        }
+
+        private void HelplineCall(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("01783 890098");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                DisplayAlert("Dialing Error", "Phone Dialer is not supported on this device.", "OK");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Phone number cannot be dialed", "Please try dialing again", "OK");
+            }
         }
     }
 }
