e00b34feec6b05eab53035f179e842adedd9f732
aowsr <35462172+aowsr@users.noreply.github.com>
2022-04-22 22:00:10 +0100

    Created a main menu and Language Selector. added styling to buttons

---
+++ Team32App/Views/LanguageSelector.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using Team32App.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Team32App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LanguageSelector : ContentPage
+    {
+        public LanguageSelector()
+        {
+            InitializeComponent();
+            picker.SelectedIndex = 0;
+        }
+
+        void OnUpdateLangugeClicked(object sender, System.EventArgs e)
+        {
+            if (picker.SelectedItem != null)
+            {
+                var language = CultureInfo.GetCultures(CultureTypes.NeutralCultures).ToList().First(element => element.EnglishName.Contains(picker.SelectedItem.ToString())); ;
+                Thread.CurrentThread.CurrentUICulture = language;
+                AppResources.Culture = language;
+                App.Current.MainPage = new NavigationPage(new MainPage());
+            }
+        }
+    }
+}
--- Team32App/Views/MainMenu.xaml.cs
+++ Team32App/Views/MainMenu.xaml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using Team32App.Resources;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +19,32 @@ namespace Team32App.Views
         {
             InitializeComponent();
         }
+
+        async void Phrases(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.Phrases());
+        }
+
+        async void Contacts(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.Contacts());
+        }
+
+        async void Locations(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.Locations());
+        }
+
+        async void Communication(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.Communication());
+        }
+
+        async void LanguageSelector(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.LanguageSelector());
+        }
+
+
     }
 }
