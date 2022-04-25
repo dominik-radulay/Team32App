65b93cdc7ef8610d316533289c1fa2328f3f344b
dominik-radulay <59743640+dominik-radulay@users.noreply.github.com>
2022-04-25 18:01:37 +0100

    Update for Lukas

--- Team32App/Views/CommunicationLevels/FormalW.xaml.cs
+++ Team32App/Views/CommunicationLevels/FormalW.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Team32App.Resources;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace Team32App.Views.CommunicationLevels
         public FormalW()
         {
             InitializeComponent();
+            CardinalNumbers.Text = AppResources.Cardinal_Numbers + "\n" + AppResources.Cardinal_Numbers;
+
         }
     }
 }
